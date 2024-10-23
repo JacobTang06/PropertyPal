cc2f11c93269c1ba239d1160c1a3c2659b08a0db
JacobTang06 <jake.tang@gmail.com>
2024-10-23 14:56:32 -0700

    Implemented Recommendations Page

---
+++ app/src/main/java/com/example/tangestate/RecommendationsAdapter.kt
@@ -0,0 +1,130 @@
+package com.example.tangestate
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+private const val ACRES_TO_SQFT = 43560
+
+class RecommendationsAdapter(
+    private val context: Context,
+    private val houses: List<House>,
+    private val viewModel: SharedViewModel,
+    private val listener: OnItemClickListener
+) : RecyclerView.Adapter<RecommendationsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.house_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val house = houses[position]
+        holder.bind(house)
+    }
+
+    override fun getItemCount() = houses.size
+
+    fun updateItemColor(position: Int, likeStatus: Boolean) {
+        val house = houses[position]
+        if (viewModel.favoriteHouseItems.value?.get(house) == true) {
+            viewModel.favoriteHouseItems.value?.set(house, false)
+            if (viewModel.favoriteHouseItems.value!!.isNotEmpty()) {
+                viewModel.favoriteHouseItems.value?.remove(house)
+            }
+        } else {
+            viewModel.favoriteHouseItems.value?.set(house, true)
+        }
+        notifyItemChanged(position)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val housePrice = itemView.findViewById<TextView>(R.id.house_price_textview)
+        private val houseBeds = itemView.findViewById<TextView>(R.id.house_beds_textview)
+        private val houseBaths = itemView.findViewById<TextView>(R.id.house_baths_textview)
+        private val houseSqft = itemView.findViewById<TextView>(R.id.house_sqft_textview)
+        private val houseAddress = itemView.findViewById<TextView>(R.id.house_address_textview)
+        private val houseStatus = itemView.findViewById<TextView>(R.id.house_status_textview)
+        private val houseImage = itemView.findViewById<ImageView>(R.id.house_image)
+        private val likeButtonOutline = itemView.findViewById<ImageView>(R.id.liked_houses_button_outline)
+        private val cardView = itemView.findViewById<CardView>(R.id.house_card_view)
+
+        fun bind(house: House) {
+            if (house.housePrice == null) {
+                housePrice.text = "Price: N/A"
+            } else {
+                val formattedPrice = "%,d".format(house.housePrice)
+                housePrice.text = "$$formattedPrice"
+            }
+            if (house.houseBaths == null) {
+                houseBaths.text = "n/a ba |"
+            } else {
+                houseBaths.text = house.houseBaths.toString() + " ba | "
+            }
+            if (house.houseBeds == null) {
+                houseBeds.text = "n/a bds |"
+            } else {
+                houseBeds.text = house.houseBeds.toString() + " bds | "
+            }
+            houseAddress.text = house.houseAddress
+            houseStatus.text = house.houseStatus?.replace("_", " ")
+
+            if (house.houseAreaUnit == "sqft") {
+                if (house.houseSqft == null) {
+                    houseSqft.text = "n/a sqft"
+                } else {
+                    houseSqft.text = house.houseSqft.toInt().toString() + " sqft"
+                }
+            } else {
+                if (house.houseSqft == null) {
+                    houseSqft.text = "n/a sqft"
+                } else {
+                    houseSqft.text =
+                        (house.houseSqft.times(ACRES_TO_SQFT)).toInt().toString() + " sqft"
+                }
+            }
+
+            Glide.with(itemView)
+                .load(house.houseImageUrl)
+                .centerCrop()
+                .into(houseImage)
+
+            if (viewModel.favoriteHouseItems.value?.get(house) == true) {
+                likeButtonOutline.setColorFilter(ContextCompat.getColor(context, R.color.lightRed))
+            } else {
+                likeButtonOutline.clearColorFilter()
+            }
+
+            likeButtonOutline.setOnClickListener {
+                if (viewModel.favoriteHouseItems.value?.get(house) == true) {
+                    likeButtonOutline.clearColorFilter()
+                    viewModel.favoriteHouseItems.value?.set(house, false)
+                    if (viewModel.favoriteHouseItems.value!!.isNotEmpty()) {
+                        viewModel.favoriteHouseItems.value?.remove(house)
+                    }
+                } else {
+                    likeButtonOutline.setColorFilter(
+                        ContextCompat.getColor(
+                            context,
+                            R.color.lightRed
+                        )
+                    )
+                    viewModel.favoriteHouseItems.value?.set(house, true)
+                }
+            }
+
+            cardView.setOnClickListener {
+                listener.onItemClick(house, viewModel.favoriteHouseItems.value?.get(house))
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/tangestate/RecommendationsFragment.kt
+++ app/src/main/java/com/example/tangestate/RecommendationsFragment.kt
@@ -1,31 +1,180 @@
 package com.example.tangestate
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestHeaders
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
+import kotlin.math.ceil
+import kotlin.math.floor
 
-class RecommendationsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+private const val TAG = "RecommendationsFragment"
+private const val ACRES_TO_SQFT = 43560
+private const val API_KEY = BuildConfig.API_KEY
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class RecommendationsFragment : Fragment(), OnItemClickListener {
+    private lateinit var sharedViewModel : SharedViewModel
+    private lateinit var recommendationsRv : RecyclerView
+    private lateinit var recommendationsAdapter : RecommendationsAdapter
+    private var houseItems = mutableListOf<House>()
+
+    private var avgPrice = 0
+    private var avgBeds = 0
+    private var avgBaths = 0.0F
+    private var avgSqft = 0.0F
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_suggestions, container, false)
+        val view = inflater.inflate(R.layout.fragment_suggestions, container, false)
+        sharedViewModel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+
+        val layoutManager = LinearLayoutManager(context)
+        recommendationsRv = view.findViewById(R.id.recommendations_rv)
+        recommendationsRv.layoutManager = layoutManager
+        recommendationsRv.setHasFixedSize(true)
+        recommendationsAdapter = RecommendationsAdapter(view.context, houseItems, sharedViewModel, this)
+        recommendationsRv.adapter = recommendationsAdapter
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        calculateHouseStatistics()
+        fetchHouses()
+        super.onViewCreated(view, savedInstanceState)
     }
 
     companion object {
         fun newInstance() : RecommendationsFragment {
             return RecommendationsFragment()
+        }
+    }
+
+    private fun calculateHouseStatistics() {
+        if(sharedViewModel.favoriteHouseItems.value!!.isNotEmpty()) {
+            var sumPrice = 0
+            var sumBeds = 0
+            var sumBaths = 0.0F
+            var sumSqft = 0.0F
+
+            for(house in sharedViewModel.favoriteHouseItems.value!!.keys) {
+                if(house != null) {
+                    sumPrice += house.housePrice!!
+                    sumBeds += house.houseBeds!!
+                    sumBaths += house.houseBaths!!
+                    sumSqft += if(house.houseAreaUnit == "sqft") {
+                        house.houseSqft!!
+                    } else {
+                        house.houseSqft!!.times(ACRES_TO_SQFT)
+                    }
+                }
+            }
+            avgPrice = sumPrice / sharedViewModel.favoriteHouseItems.value!!.size
+            avgBeds = sumBeds / sharedViewModel.favoriteHouseItems.value!!.size
+            avgBaths = sumBaths / sharedViewModel.favoriteHouseItems.value!!.size
+            avgSqft = sumSqft / sharedViewModel.favoriteHouseItems.value!!.size
+        }
+        else {
+            Toast.makeText(context, "No houses liked, statistics set to default values", Toast.LENGTH_LONG).show()
+            avgPrice = 500000
+            avgBeds = 2
+            avgBaths = 2.0F
+            avgSqft = 2000F
+        }
+    }
+
+    private fun fetchHouses() {
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        val headers = RequestHeaders()
+
+        headers["x-rapidapi-key"] = API_KEY
+        headers["x-rapidapi-host"] = "zillow-com1.p.rapidapi.com"
+
+        val clientUrl = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
+
+        params["location"] = "New York, NY; Los Angeles, CA; Mountain View, CA; Austin, TX; Naples, FA"
+        params["minPrice"] = (avgPrice - avgPrice / 10).toString()
+        params["maxPrice"] = (avgPrice + avgPrice / 10).toString()
+        params["minBeds"] = avgBeds.toString()
+        params["maxBeds"] = (avgBeds + 1).toString()
+        params["minBaths"] = floor(avgBaths).toString()
+        params["maxBaths"] = ceil(avgBaths).toString()
+        params["minSqft"] = (avgSqft - avgSqft / 10.0F).toString()
+        params["maxSqft"] = (avgSqft + avgSqft / 10.0F).toString()
+        params["page"] = "1"
+
+        client.get(clientUrl, headers, params, object: JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure $response")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess: JSON properties data $json")
+
+                try {
+                    val parsedJson = createJson().decodeFromString(
+                        BaseResponse.serializer(),
+                        json.jsonObject.toString()
+                    )
+                    if(parsedJson.properties!!.isEmpty()) {
+                        Toast.makeText(context, "No houses were found with the specified filters", Toast.LENGTH_LONG).show()
+                    }
+                    parsedJson.properties.let { list ->
+                        houseItems.addAll(list)
+                        recommendationsAdapter.notifyDataSetChanged()
+                    }
+                } catch (e: JSONException) {
+                    Log.e(TAG, "Exception: $e")
+                }
+            }
+        })
+    }
+
+    private val REQUEST_CODE = 1001
+
+    override fun onItemClick(house: House, likeStatus: Boolean?) {
+        val intent = Intent(context, HouseDetailsActivity::class.java).apply {
+            putExtra("HOUSE_EXTRA", house)
+            putExtra("LIKE_STATUS", likeStatus)
+            putExtra("HOUSE_POSITION", houseItems.indexOf(house))
+        }
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val likeStatus = data?.getBooleanExtra("LIKE_STATUS", false)
+            val position = data?.getIntExtra("HOME_POSITION", -1)
+            Log.d("Clicked house data maintained", likeStatus.toString() + ", " + position.toString())
+            if (position != null && likeStatus != null) {
+                recommendationsAdapter.updateItemColor(position, likeStatus)
+            }
+            else {
+                error("Attempting to access invalid house index")
+            }
         }
     }
 }
